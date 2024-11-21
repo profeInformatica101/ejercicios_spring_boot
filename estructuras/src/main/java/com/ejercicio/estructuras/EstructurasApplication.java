242a2f71cae3004c610ac98ab699724c3c0b7ede
Pablo <jdommay863@g.educaand.es>
2024-11-22 00:45:12 +0100

    Proyecto Inicial Estructura Maven Java para Ejercicios

---
+++ estructuras/src/main/java/com/ejercicio/estructuras/EstructurasApplication.java
@@ -0,0 +1,13 @@
+package com.ejercicio.estructuras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstructurasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstructurasApplication.class, args);
+	}
+
+}
---
+++ estructuras/src/test/java/com/ejercicio/estructuras/EstructurasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejercicio.estructuras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstructurasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
