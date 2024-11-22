4225e5048d6475a954424274b1d69e81cbc46505
ServandoT <gonzalez.agar.carlos@iescamas.es>
2024-11-22 09:46:45 +0100

    Añadido ejercicio-2

--- estructuras/src/main/java/com/ejercicio/estructuras/ControladorEjercicios.java
+++ estructuras/src/main/java/com/ejercicio/estructuras/ControladorEjercicios.java
@@ -5,13 +5,28 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
+
 @RestController
 public class ControladorEjercicios {
     
     @PostMapping("/parImpar")
     public String ejercicio1(@RequestParam int numero) {
-        String ret = "Hola Mundo";
-        return ret;
+        boolean esPar = numero % 2 == 0;
+        return esPar ? "<h2>Es par</h2>" : "<h2>Es impar</h2>";
+    }
+
+    @PostMapping("/tablaMultiplicar")
+    public String ejercicio2(@RequestParam int numero) {
+        StringBuilder res = new StringBuilder();
+        res.append("<ul>");
+
+        for (int i = 1; i <= 10; i++) {
+            res.append("<li>" + numero + " x " + i + " = " + (i * numero) + "</l1>");
+        }
+
+        res.append("</ul>");
+
+        return res.toString();
     }
     
 }
